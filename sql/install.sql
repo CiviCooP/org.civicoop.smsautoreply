93d77c5ac5c2e2891f240982054ecdf69725affb
Jaap Jansma <info@edeveloper.nl>
2014-06-24 18:58:39 +0200

    added is active

--- sql/install.sql
+++ sql/install.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS `civicrm_sms_autoreply` (
   `keyword` varchar(160) NOT NULL,
   `reply` varchar(255) NOT NULL,
   `provider_id` int(11) DEFAULT NULL,
+  `is_active` INT(11) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
