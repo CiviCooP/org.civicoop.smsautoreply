2c01c7a904d6619f92b2ac80941c43f85b48b370
Jaap Jansma <info@edeveloper.nl>
2014-06-25 14:58:54 +0200

    update processing of contacts

--- CRM/Smsautoreply/Reply.php
+++ CRM/Smsautoreply/Reply.php
@@ -36,14 +36,14 @@ class CRM_Smsautoreply_Reply {
    * @param type $objectRef
    */
   public function post($op, $objectName, $objectId, &$objectRef) {
-    CRM_Core_Error::debug_log_message('Post hook');
     if ($op == 'create' && $objectName == 'Activity') {
-      CRM_Core_Error::debug_log_message('Post hook valid activitty: '.$objectRef->subject);
       //check if subject is valid
       if ($this->isValidActivity($objectRef)) {
         //ok this is an incoming sms
-        CRM_Core_Error::debug_log_message('Post hook valid subject');
-        $this->process($objectRef->details, $objectRef->phone_number, $objectRef->target_contact_id, $objectRef->source_contact_id);
+        $target_contact_ids = CRM_Activity_BAO_ActivityTarget::retrieveTargetIdsByActivityId($objectRef->id);
+        if (count($target_contact_ids)) {
+          $this->process($objectRef->details, $objectRef->phone_number, $target_contact_ids, $objectRef->source_contact_id);
+        }        
       }
     }
   }
@@ -70,17 +70,14 @@ class CRM_Smsautoreply_Reply {
    * @param type $from_contact_id
    * @param type $to_contact_id
    */
-  protected function process($message, $from_phone, $from_contact_id, $to_contact_id) {
-    
-    CRM_Core_Error::debug_log_message('Processing autoreplies for '.$message.' from '.$from_phone . ' (cid: '.$from_contact_id.')');
-    
+  protected function process($message, $from_phone, $from_contact_ids, $to_contact_id) {    
     $sql = 'SELECT * FROM `civicrm_sms_autoreply` WHERE %1 LIKE CONCAT(`keyword`, "%") AND `is_active` = "1"';
     $replies = CRM_Core_DAO::executeQuery($sql, array(
           1 => array($message, 'String'),
     ));
 
     while ($replies->fetch()) {
-      $this->reply($replies->reply, $from_phone, $from_contact_id, $replies->provider_id, $replies->charge, $replies->financial_type_id, $dao->subject);
+      $this->reply($replies->reply, $from_phone, $from_contact_ids, $replies->provider_id, $replies->charge, $replies->financial_type_id, $replies->subject);
     }
   }
 
@@ -95,12 +92,11 @@ class CRM_Smsautoreply_Reply {
    * @param type $charge
    * @param type $financial_type_id
    */
-  protected function reply($body, $to_phone, $to_contact_id, $provider_id, $from_contact_id, $charge, $financial_type_id, $subject) {
+  protected function reply($body, $to_phone, $to_contact_ids, $provider_id, $from_contact_id, $charge, $financial_type_id, $subject) {
     
     CRM_Core_Error::debug_log_message('Send reply '.$subject.' to '.$to_phone);
     
-    $contact_ids[] = $to_contact_id;
-    $contactDetails[] = $this->getContactDetails($to_contact_id, $to_phone);
+    $contactDetails[] = $this->getContactDetails($to_contact_ids, $to_phone);
     $activityParams['text_message'] = $body;
     $activityParams['activity_subject'] = $subject;
     $smsParams['provider_id'] = $provider_id;
@@ -111,10 +107,10 @@ class CRM_Smsautoreply_Reply {
       $smsParams['financial_type_id'] = $financial_type_id;
     }
 
-    list($sent, $activityId, $countSuccess) = CRM_Activity_BAO_Activity::sendSMS($contactDetails, $activityParams, $smsParams, $contact_ids, $from_contact_id);
+    list($sent, $activityId, $countSuccess) = CRM_Activity_BAO_Activity::sendSMS($contactDetails, $activityParams, $smsParams, $to_contact_ids, $from_contact_id);
   }
 
-  protected function getContactDetails($contact_id, $phone) {
+  protected function getContactDetails($contactIds, $phone) {
     $returnProperties = array(
       'sort_name' => 1,
       'phone' => 1,
@@ -122,20 +118,18 @@ class CRM_Smsautoreply_Reply {
       'is_deceased' => 1,
       'display_name' => 1,
     );
-    $contactIds[] = $contact_id;
-    list($contactDetails) = CRM_Utils_Token::getTokenDetails($contactIds, $returnProperties, FALSE, FALSE);
-
-    $value = $contactDetails[$contact_id];
-
-    //to check if the phone type is "Mobile"
-    $phoneTypes = CRM_Core_OptionGroup::values('phone_type', TRUE, FALSE, FALSE, NULL, 'name');
-
-    $formatFrom = $this->formatPhone($this->stripPhone($from), $like, "like");
-    $escapedFrom = CRM_Utils_Type::escape($formatFrom, 'String');
-    $contactDetails[$contact_id]['phone_id'] = CRM_Core_DAO::singleValueQuery('SELECT id FROM civicrm_phone WHERE phone LIKE "' . $escapedFrom . '"');
-    $contactDetails[$contact_id]['phone'] = $phone;
-    $contactDetails[$contact_id]['phone_type_id'] = CRM_Utils_Array::value('Mobile', $phoneTypes);
     
+    list($contactDetails) = CRM_Utils_Token::getTokenDetails($contactIds, $returnProperties, FALSE, FALSE);
+    foreach($contactIds as $contact_id) {
+      //to check if the phone type is "Mobile"
+      $phoneTypes = CRM_Core_OptionGroup::values('phone_type', TRUE, FALSE, FALSE, NULL, 'name');
+
+      $formatFrom = $this->formatPhone($this->stripPhone($phone), $like, "like");
+      $escapedFrom = CRM_Utils_Type::escape($formatFrom, 'String');
+      $contactDetails[$contact_id]['phone_id'] = CRM_Core_DAO::singleValueQuery('SELECT id FROM civicrm_phone WHERE phone LIKE "' . $escapedFrom . '"');
+      $contactDetails[$contact_id]['phone'] = $phone;
+      $contactDetails[$contact_id]['phone_type_id'] = CRM_Utils_Array::value('Mobile', $phoneTypes);
+    }
     return $contactDetails;
   }
   
